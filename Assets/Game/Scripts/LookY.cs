8784a8aa2e7a16e28361f19ca91ffb1806c1984e
azarrias <adolfo.zarrias@gmail.com>
2018-09-25 21:19:28 +0200

    Add mouse look, create pivot for Y rotation and finish character controller

---
+++ Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour {
+
+    private float sensitivity = .5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x -= mouseY * sensitivity;
+        transform.localEulerAngles = newRotation;
+	}
+}
